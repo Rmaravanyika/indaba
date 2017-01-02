08291ba29fa387f8231c361f30a51e613ff4d018
hamub <hbutau35@gmail.com>
2017-01-02 13:52:44 +0200

    modified views

--- proposals/forms.py
+++ proposals/forms.py
@@ -10,38 +10,21 @@ from crispy_forms.bootstrap import FormActions
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, HTML
 from markitup.widgets import MarkItUpWidget
-from easy_select2.widgets import Select2Multiple
+# from easy_select2.widgets import Select2Multiple
 
-from wafer.talks.models import Talk, render_author
-
-
-def get_proposal_form_class():
-    pass
+from proposals.models import Proposal
 
 class ProposalForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        initial = kwargs.setdefault('initial', {})
-
-        super(ProposalForm, self).__init__(*args, **kwargs)
-
-        self.helper = FormHelper(self)
-        submit_button = Submit('submit', _('Submit'))
-        instance = kwargs['instance']
-        if instance:
-            self.helper.layout.append(
-                FormActions(
-                    submit_button,
-                    HTML('<a href="%s" class="btn btn-danger">%s</a>'
-                         % (reverse('wafer_talk_delete', args=(instance.pk,)),
-                            _('Delete')))))
-        else:
-            self.helper.add_input(submit_button)
-
     class Meta:
         model = Proposal
         fields = ('title', 'talk_type', 'abstract')
         widgets = {
             'abstract': MarkItUpWidget(),
-            'notes': forms.Textarea(attrs={'class': 'input-xxlarge'}),
-            'authors': Select2Multiple(),
         }
+    def __init__(self, *args, **kwargs):
+        super(ProposalForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        submit_button = Submit('submit', _('Submit'))
+
+
--- proposals/views.py
+++ proposals/views.py
@@ -2,20 +2,18 @@ from django.shortcuts import render
 from django.views.generic import (TemplateView, CreateView,)
 
 from proposals.models import   Proposal, Talk_Type
+from proposals.forms import ProposalForm
 
 
 class CreateProposal(CreateView):
     model = Proposal
+    form_class = ProposalForm
     template_name = "proposals/talk_form.html"
-    fields = ['title', 'abstract','talk_type']
 
-    # def get_form_class(self):
-    #     return get_proposal_form_class()
+    def get_form_kwargs(self):
+        kwargs = super(CreateProposal, self).get_form_kwargs()
+        return kwargs
 
-    # def get_form_kwargs(self):
-    #     kwargs = super(CreateProposal, self).get_form_kwargs()
-    #     return kwargs
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(CreateProposal, self).get_context_data(**kwargs)
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(CreateProposal, self).get_context_data(**kwargs)
+        return context
